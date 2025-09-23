cc1c3d658115caa3b4d3a60bf1a1fa171a46d4fa
ArtyomRomanow <Official.romanow@yandex.ru>
2025-09-23 18:33:45 +0700

    Add files via upload

--- kalkulation.py
+++ kalkulation.py
@@ -1,34 +1,42 @@
-while True:
-    print('Выберите операцию:')
-    print('1. Сложение')
-    print('2. Вычитание')
-    print('3. Умножение')
-    print('4. Деление')
-    print('5. Завершить операцию')
+def main(input_str: str) -> str:
+    parts = input_str.strip().split()
+    if len(parts) != 3:
+        print("Некорректный формат ввода")
 
-    choice = input('Введите номер операции: ')
+    a_str, op, b_str = parts
 
-    if choice in ('1', '2', '3', '4', '5'):
-        if choice == '5':
-            print("Завершение программы.")
+    if not (a_str.isdigit() and b_str.isdigit()):
+        print("Операнды должны быть целыми числами")
+
+    a = int(a_str)
+    b = int(b_str)
+
+    if not (1 <= a <= 10 and 1 <= b <= 10):
+        print("Числа должны быть от 1 до 10 включительно")
+
+    if op == '+':
+        result = a + b
+    elif op == '-':
+        result = a - b
+    elif op == '*':
+        result = a * b
+    elif op == '/':
+        if b == 0:
+            print("Деление на ноль")
+        result = a // b
+    else:
+        print("Неверный оператор")
+
+    return str(result)
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            user_input = input()
+            print(main(user_input))
+        except Exception as e:
+            print(f"throws Exception")
             break
 
-        a = int(input("Введите первое число (от 1 до 10): "))
-        b = int(input("Введите второе число (от 1 до 10): "))
 
-        if 1 <= a <= 10 and 1 <= b <= 10:
-            if choice == '1':
-                print('Сумма чисел равна', a + b)
-            elif choice == '2':
-                print('Разность чисел равна', a - b)
-            elif choice == '3':
-                print('Произведение чисел равно', a * b)
-            elif choice == '4':
-                if b != 0:
-                    print('Частное чисел равно', a // b)
-                else:
-                    print('Ошибка: Деление на ноль!')
-        else:
-            print("Ошибка: Оба числа должны быть от 1 до 10 включительно.")
-    else:
-        print('Введите корректные данные.')
